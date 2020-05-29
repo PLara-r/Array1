e8ef4258a798f6aa6bb1158984cc6eae696e702c
PLara-r <tl1226932@gmail.com>
2020-05-29 14:39:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String alpha = "";
+        for(char current = 'a'; current <= 'z'; current++)
+        alpha += current;
+        System.out.println(alpha);//abcdefghijklmnopqrstuvwxyz
+
+    }
+}
