e29acb0ebd1c2fd66f42a1c96c5555903ea6cf46
PLara-r <tl1226932@gmail.com>
2020-05-30 14:28:13 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
-        String alpha = "";
-        for(char current = 'a'; current <= 'z'; current++)
-        alpha += current;
-        System.out.println(alpha);//abcdefghijklmnopqrstuvwxyz
+        public static void main(String[] args)
+        {int[] numbers = new int[10];
+             for (int i = 0; i < numbers.length; i++)
+              numbers[i] = i + 5;
 
-    }
-}
+            System.out.println (numbers[5]);
+
+            int[] numbers1 = { 6, 9, 1 };
+            Arrays.sort(numbers1);
+            for (int i = 0; i < numbers1.length; i++)
+                  System.out.println (numbers1[i] +  " ");
+
+
+        }}
